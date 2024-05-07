b58cff36d0e0a42d0e9c88f40a53d9eb95e79647
Kelvin <kelvingraddick@kg.codes>
2024-05-07 02:11:57 -0400

    Initial commit

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, Text, useColorScheme, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { colors } from './constants/colors';
+
+function HomeScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
---
+++ constants/colors.js
@@ -0,0 +1,12 @@
+export const colors = {
+  white: '#fff',
+  black: '#000',
+  darkGray: '#1D2027',
+  lightGray: '#dfdfdf',
+  whiteGray: '#f5f5f5',
+  blue: '#5C63FF',
+  yellow: '#FAD275',
+  green: '#5FBFA2',
+  orange: '#FBA174',
+  gray: '#999999',
+};
