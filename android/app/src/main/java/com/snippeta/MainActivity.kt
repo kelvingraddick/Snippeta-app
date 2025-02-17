1f44258e2334969e430c5f1927b14bc3f6bfe6d0
Kelvin <kelvingraddick@kg.codes>
2025-02-17 14:06:05 -0500

    Update Android application ID and namespace

--- android/app/src/main/java/com/snippeta/MainActivity.kt
+++ android/app/src/main/java/com/snippeta/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.snippeta
+package com.wavelinkllc.snippeta
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/snippeta/MainApplication.kt
+++ android/app/src/main/java/com/snippeta/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.snippeta
+package com.wavelinkllc.snippeta
 
 import android.app.Application
 import com.facebook.react.PackageList
