2576098595a6a5f468ca9b719efcf05625db70fe
Kelvin <kelvingraddick@kg.codes>
2025-09-10 02:42:23 -0400

    Android: fix build issues with custom keyboard installation detection

--- android/app/src/main/java/com/snippeta/KeyboardBridgePackage.kt
+++ android/app/src/main/java/com/snippeta/KeyboardBridgePackage.kt
@@ -1,4 +1,4 @@
-package com.snippeta
+package com.wavelinkllc.snippeta
 
 import com.facebook.react.ReactPackage
 import com.facebook.react.bridge.NativeModule
--- android/app/src/main/java/com/snippeta/KeyboardNativeModule.kt
+++ android/app/src/main/java/com/snippeta/KeyboardNativeModule.kt
@@ -1,4 +1,4 @@
-package com.snippeta
+package com.wavelinkllc.snippeta
 
 import android.content.Context
 import android.view.inputmethod.InputMethodManager
