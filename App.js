577460f471e0d8c760d273e755bf6d60801b5414
Kelvin <kelvingraddick@kg.codes>
2025-09-22 01:36:53 -0400

    iPad optimizations: fixed UI/UX issues with pop up modals

--- App.js
+++ App.js
@@ -551,7 +551,7 @@ export default function App() {
                     options={{ headerShown: false }}
                   />
                 </Stack.Group>
-                <Stack.Group screenOptions={{ presentation: 'modal' }}>
+                <Stack.Group screenOptions={{ presentation: Platform.isPad ? 'fullScreenModal' : 'modal' }}>
                   <Stack.Screen
                     name="Keyboard"
                     component={KeyboardScreen}
