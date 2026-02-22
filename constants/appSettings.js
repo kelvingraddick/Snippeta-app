27d3be3eb049309f67d7f116e012f3a159c6f9e7
Kelvin <kelvingraddick@kg.codes>
2026-02-22 17:22:50 -0500

    macOS version 2.7 build 46

--- constants/appSettings.js
+++ constants/appSettings.js
@@ -1,7 +1,7 @@
 import { LOG_LEVEL as REVENUE_CAT_LOG_LEVEL } from 'react-native-purchases';
 export const appSettings = {
   VERSION_NUMBER: '2.7',
-  BUILD_NUMBER: 45,
+  BUILD_NUMBER: 46,
   SENTRY_ENABLED: true,
   SENTRY_DSN_URL: '{{SENTRY_DSN_URL}}',
   SENTRY_AUTH_TOKEN: '{{SENTRY_AUTH_TOKEN}}',
